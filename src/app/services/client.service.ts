2d0189f937767fc61baba316b35be9bea9287b34
BrunoTZNR <brunomarcodasilva@gmail.com>
2023-11-03 00:47:17 -0300

    feat: prepare client service

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Client } from '../types/Client.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,64 +7,5 @@ export class ClientService {
 
   constructor() { }
 
-  getClient(id: number): Client {
-    return this.getClients()[id];
-  }
 
-  getClients(): Client[] {
-    return [
-      {
-        id: 1,
-        cpf: '12345678901',
-        fname: 'Bruno',
-        lname: 'Silva',
-        dtNasc: '01/01/2003',
-        phone: '61123456789',
-        whatsapp: '61123456789',
-        email: 'admin@admin.com',
-        rua: 'aquela rua',
-        casa: 'naquela casa',
-        cep: '12345678'
-      },
-      {
-        id: 2,
-        cpf: '12345678901',
-        fname: 'Bianca',
-        lname: 'Chagas',
-        dtNasc: '14/09/2003',
-        phone: '61123456789',
-        whatsapp: '61123456789',
-        email: 'admin@admin.com',
-        rua: 'aquela rua',
-        casa: 'naquela casa',
-        cep: '12345678'
-      },
-      {
-        id: 3,
-        cpf: '12345678901',
-        fname: 'Renan',
-        lname: 'Rocha',
-        dtNasc: '11/01/2003',
-        phone: '61123456789',
-        whatsapp: '61123456789',
-        email: 'admin@admin.com',
-        rua: 'aquela rua',
-        casa: 'naquela casa',
-        cep: '12345678'
-      },
-      {
-        id: 4,
-        cpf: '12345678901',
-        fname: 'Italo',
-        lname: 'Oliveira',
-        dtNasc: '06/01/2003',
-        phone: '61123456789',
-        whatsapp: '61123456789',
-        email: 'admin@admin.com',
-        rua: 'aquela rua',
-        casa: 'naquela casa',
-        cep: '12345678'
-      }
-    ]
-  }
 }
